d6019d721f30ec46a64db00eaf56e72328e93a3e
Ha Quanq <quangha0120010@gmail.com>
2024-07-31 11:25:19 +0700

    :beginner: First commit

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {},
+    fontWeight: {},
+    borderRadius: {
+      flipper: "var(--rounded-flipper)",
+      full: "9999px",
+    },
+    colors: {
+      red: {
+        300: "var(--clr-red-300)",
+        500: "var(--clr-red-500)",
+      },
+      blue: {
+        100: "var(--clr-blue-100)",
+        500: "var(--clr-blue-500)",
+        800: "var(--clr-blue-800)",
+        900: "var(--clr-blue-900)",
+      },
+      white: "var(--clr-white)",
+    },
+    screens: {
+      md: "36em",
+      lg: "48em",
+    },
+    extend: {
+      height: {
+        "2/1": "200%",
+      },
+    },
+  },
+  plugins: [],
+};
